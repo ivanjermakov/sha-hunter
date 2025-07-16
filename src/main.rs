8b7ffe392bcfa6434260967d3ff2c901f4938b6e
ivanjermakov <ivanjermakov1@gmail.com>
2025-07-17 00:48:52 +0200

    Rand strings; raw speed

--- src/main.rs
+++ src/main.rs
@@ -2,52 +2,51 @@ use std::{
     cmp::Ordering,
     env,
     process::exit,
-    sync::{Arc, Mutex},
+    ptr,
     thread::{available_parallelism, spawn},
 };
 
 use hex_literal::hex;
+use rand::distr::SampleString;
+use rand_distr::Alphanumeric;
 use sha2::{Digest, Sha256};
 
 mod hex;
 
 fn main() {
-    let min = Arc::new(Mutex::new([1; 32]));
     let cpus = available_parallelism().unwrap().into();
-    println!("utilizing {cpus} threads");
-
     let handles = (0..cpus)
         .map(|n| {
             let prefix = env::args().nth(1).expect("no prefix arg");
             let goal = hex!("000000000F");
             let cpu_flag = format!("{:02x}", n).into_bytes();
-            let min = min.clone();
 
             spawn(move || {
-                for i in 0..u64::MAX {
-                    let data = [
-                        prefix.as_bytes(),
-                        &cpu_flag,
-                        &format!("{:02x}", i).into_bytes(),
-                    ]
-                    .concat();
+                let mut rng = rand::rng();
+                loop {
+                    let mut data: [u8; 20] = [47; 20];
+                    unsafe {
+                        ptr::copy_nonoverlapping(
+                            prefix.as_bytes().as_ptr(),
+                            data[0..13].as_mut_ptr(),
+                            13,
+                        );
+                        ptr::copy_nonoverlapping(cpu_flag.as_ptr(), data[13..15].as_mut_ptr(), 2);
+                        let take = Alphanumeric.sample_string(&mut rng, 5);
+                        ptr::copy_nonoverlapping(take.as_ptr(), data[15..20].as_mut_ptr(), 5);
+                    }
 
                     let mut hasher = Sha256::new();
-                    hasher.update(&data);
+                    hasher.update(data);
                     let hash = hasher.finalize();
 
-                    if hash[..].cmp(min.lock().unwrap().as_ref()) == Ordering::Less {
-                        let v = min.clone();
-                        let mut v = v.lock().unwrap();
-                        *v = hash[..].try_into().unwrap();
+                    if hash[..].cmp(&goal) == Ordering::Less {
                         println!(
-                            "{: <32} {}",
-                            String::from_utf8(data).unwrap(),
+                            "{: <24} {}",
+                            String::from_utf8(Vec::from(data)).unwrap(),
                             chunk(&hex::hex(&hash), 8)
                         );
-                        if hash[..].cmp(&goal) == Ordering::Less {
-                            exit(0);
-                        }
+                        exit(0);
                     }
                 }
             })
